c8970e8b3ba7afd73e342a78d04520e7a599b0e5
Tomi <tomiatschool@gmail.com>
2024-03-08 12:44:17 +0100

    Add project files.

---
+++ Keszthely/Keszthely.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keszthely
+{
+    internal class Keszthely
+    {
+        public string Nev { get; set; }
+        public int SzuletesiEv { get; set; }
+        public int Rajtszam { get; set; }
+        public string Neme { get; set; }
+        public string Eletkor { get; set; }
+        public TimeSpan Uszas { get; set; }
+        public TimeSpan ElsoDepo { get; set; }
+        public TimeSpan Kerekpar { get; set; }
+        public TimeSpan MasodikDepo { get; set; }
+        public TimeSpan Futas { get; set; }
+        public TimeSpan Osszesido { get; set; }
+        public Keszthely(string s)
+        {
+            string[] v = s.Split(';');
+            Nev = v[0];
+            SzuletesiEv = int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            Neme = v[3];
+            Eletkor = v[4];
+            Uszas = TimeSpan.Parse(v[5]);
+            ElsoDepo = TimeSpan.Parse(v[6]);
+            Kerekpar = TimeSpan.Parse(v[7]);
+            MasodikDepo = TimeSpan.Parse(v[8]);
+            Futas = TimeSpan.Parse(v[9]);
+        }
+        public override string ToString()
+        {
+            return $"Név: {Nev}, Születési év:{SzuletesiEv}, Rajtszám:{Rajtszam}, Neme:{Neme}, Életkor:{Eletkor}, Úszás:{Uszas},Első Depó:{ElsoDepo}, Kerékpár:{Kerekpar}, Második Depó:{MasodikDepo},Futás:{Futas}";
+        }
+    }
+}
---
+++ Keszthely/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Keszthely
+{
+    internal class Program
+    {
+        
+        static double atlag(List<Keszthely> a)
+        { 
+        var f1=a.Average(x=>x.SzuletesiEv = 2014 - x.SzuletesiEv);
+            return f1;
+        }
+        static void Main(string[] args)
+        {
+            var keszthely = new List<Keszthely>();
+            using(var sr=new StreamReader(@"..\..\..\src\Eredmenyek.txt"))
+            {
+                while(!sr.EndOfStream) 
+                {
+                    keszthely.Add(new Keszthely(sr.ReadLine()));
+                }
+                foreach (var item in keszthely)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("2.feladat");
+
+                Console.WriteLine($"A versenyt {keszthely.Count} versenyző fejezte be.");
+
+                Console.WriteLine("3.feladat");
+                int elit = 0;
+                for (int i = 0; i < keszthely.Count; i++) 
+                {
+                    if (keszthely[i].Eletkor.Contains("elit junior"))
+                    { 
+                    elit++;
+                    }
+                }
+                Console.WriteLine($"Versenyzők száma az elit junior kategóriában: {elit} fő");
+
+                Console.WriteLine("4.feladat");
+
+                Console.WriteLine($"Atlag életkor: {atlag(keszthely):0.0}év");
+
+                Console.WriteLine("5.feladat");
+
+                Console.WriteLine("Adj meg egy kategóriát: ");
+                string kategoria = (Console.ReadLine());
+                var otos = keszthely.Where(x => x.Eletkor == kategoria).Select(x => x.Rajtszam);
+                if (otos.Any())
+                {
+                    Console.WriteLine($"Rajtszamok:");
+                    Console.WriteLine(string.Join(" ", otos));
+                }
+                else
+                {
+                    Console.WriteLine("Nincs ilyen kategória");
+                }
+
+
+                Console.WriteLine("6.feladat");
+                Console.WriteLine($"A legjobb idő {keszthely.Where(x=> x.Neme=="n").OrderBy(x=>x.Osszesido).First()}");
+              
+            }
+        }
+    }
+}
